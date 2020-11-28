a3ef0fdeddcc76cc6f891f038f458b4245135c0b
patri <patri@PATRIKSPC>
2020-11-28 11:52:30 +0200

    added error display in index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-﻿<!doctype html>
+﻿<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+?>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
